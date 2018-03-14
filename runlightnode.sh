fb6987fa364b3137bf828e74a09c9bd480f0bd3c
bill clinton <ahester57@gmail.com>
2018-03-14 16:16:55 -0500

    better runlightnode.sh

--- runlightnode.sh
+++ runlightnode.sh
@@ -1,15 +1,16 @@
 # !/bin/bash
 
 CURR=`dirname $0`
-if [ -d $CURR/lightnode/geth ]; then
-geth --identity "lightnode" --networkid 9111 \
-     --datadir $CURR/lightnode  \
+DATADIR=$CURR/lightnode
+if [ -d $DATADIR/geth ]; then
+geth --identity "billclinton" --networkid 9111 \
+     --datadir $DATADIR  \
 	 --nodiscover \
 	 --syncmode "fast" \
-     --ipcpath $CURR/lightnode/geth.ipc \
+     --ipcpath $DATADIR/geth.ipc \
      --port "39908" --rpc --rpcport "42023" \
      --rpcapi "db,eth,web3,net,personal,miner" \
-     --unlock 0 --password $CURR/lightnode/passwd.sec \
+     --unlock 0 --password $DATADIR/passwd.sec \
 	 console
 else
 	echo "Initialize first"
