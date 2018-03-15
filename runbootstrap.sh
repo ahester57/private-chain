10b20b8260c3f6aafb6a8286bf11bb938706e4ee
Austin Hester <ahester57@gmail.com>
2018-03-15 14:41:50 -0500

    fix stupid mistake

--- runbootstrap.sh
+++ runbootstrap.sh
@@ -8,8 +8,7 @@ geth --identity "bootstrap" --networkid 9111 \
      --ipcpath /home/$USER/.ethereum/geth.ipc \
      --port "39909" --rpc --rpcport "42024" \
      --rpcapi "db,eth,web3,net,personal,miner" \
-     --unlock "0xd6774f7daa52d2f38a9327ad8a4dd6f2f4e3f48b" \
-	 --password $CURR/bootstrap/passwd.sec \
+     --unlock 0 --password $CURR/bootstrap/passwd.sec \
 	 console
 else
 	echo "Initialize first"
