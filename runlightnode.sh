28da58e64af418f64817eee58635e73745132879
Austin Hester <ahester57@gmail.com>
2018-03-19 15:13:42 -0500

    stupid git stuff

--- runlightnode.sh
+++ runlightnode.sh
@@ -3,7 +3,6 @@
 # Directory stuff
 CURR=`dirname $0`
 DATADIR=$CURR/lightnode
-<<<<<<< HEAD
 
 # GETH options 
 IDENTITY="lightnode"
@@ -21,18 +20,6 @@ geth --identity $IDENTITY --networkid $NETWORKID \
      --unlock 0 --password $DATADIR/passwd.sec \
 	console
      #--unlock $COINBASE --password $DATADIR/passwd.sec \
-=======
-if [ -d $DATADIR/geth ]; then
-geth --identity "billclinton" --networkid 9111 \
-     --datadir $DATADIR  \
-	 --nodiscover \
-	 --syncmode "fast" \
-     --ipcpath $DATADIR/geth.ipc \
-     --port "39908" --rpc --rpcport "42023" \
-     --rpcapi "db,eth,web3,net,personal,miner" \
-     --unlock 0 --password $DATADIR/passwd.sec \
-	 console
->>>>>>> fb6987fa364b3137bf828e74a09c9bd480f0bd3c
 else
 	echo "Initialize first"
 fi
