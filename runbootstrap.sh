3b2170664bb3f2bd2ff49e128ac793e0bc133259
Austin Hester <ahester57@gmail.com>
2018-03-19 15:45:11 -0500

    update static-nodes

--- runbootstrap.sh
+++ runbootstrap.sh
@@ -4,7 +4,7 @@ CURR=`dirname $0`
 if [ -d $CURR/bootstrap/geth ]; then
 geth --identity "bootstrap" --networkid 9111 \
      --datadir $CURR/bootstrap  \
-	 --nodiscover --syncmode "full" \
+	 --nodiscover --syncmode "fast" \
      --ipcpath /home/$USER/.ethereum/geth.ipc \
      --port "39909" --rpc --rpcport "42024" \
      --rpcapi "db,eth,web3,net,personal,miner" \
