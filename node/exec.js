703c470df735d8508b841693890e2cb4d72b4c60
Austin Hester <ahester57@gmail.com>
2018-04-09 23:24:34 -0500

    setting up for easier contract migrations

--- node/exec.js
+++ node/exec.js
@@ -1,37 +1,48 @@
-var exec = require('child_process').exec
-var Web3 = require('web3')
-var loadChainMessage = require('./loadChainMessage.js')
+const exec = require('child_process').exec
+const Web3 = require('web3')
+const ChainMessageArt = require('./ChainMessage.json')
 
-var provider = 'ws://localhost:35355'
+const networkId = '9111'
+const provider = 'ws://localhost:35355'
 
 //if (typeof web3 !== 'undefined')
 //	web3 = new Web3(web3.currentProvider);
 //else
-	web3 = new Web3(new Web3.providers.WebsocketProvider(provider));
+const web3 = new Web3(new Web3.providers.WebsocketProvider(provider));
 
-// get the contract instance
-var message = loadChainMessage();
 
-// interact
-var cb = web3.eth.personal.getAccounts().then(function (ac) {
-	return ac[0];
-}).then(function (cb) {
-	console.log(cb);
-	var call = message.methods['getMessage(address)'](cb);
-	call.call().then( function(msg) {
-		console.log(msg);
-	});
-});
 
-// watch a contract
 web3.eth.net.getId().then(function(networdId) {
+		// get the contract instance
+		const address = ChainMessageArt.networks[networkId].address;
+		//var message = loadChainMessage();
+		const message = new web3.eth.Contract(ChainMessageArt.abi, address);
 
-		message.events['SentMessage(address,string)'](function(error, event) {
+		// interact
+		var cb = web3.eth.personal.getAccounts().then(function (ac) {
+			return ac[0];
+		}).then(function (cb) {
+			console.log(cb);
+//			var call = message.methods['getMessage(address)'](cb);
+//			call.call().then( function(msg) {
+//				console.log(msg);
+//			});
+		});
+
+		console.log(message)
+		// watch for event SentMessage
+		message.events['SentMessage'] 
+		(function(error,event) {
 			console.log(error)
 		})
 		.on('data', function(log) {
-			let {returnValues: {_to, _msg}, blockNumber} = log
-			console.log(log);
+			var info = log.returnValues;
+			var from = info._from;
+			var to = info._to;
+			var msg = info._msg;
+			console.log("from: ", from);
+			console.log("to: ", to);
+			console.log("msg: ", msg);
 		}).on('changed', function(log) {
 			console.log(log);
 		}).on('error', function(log) {
--- scripts/loadChainMessage.js
+++ scripts/loadChainMessage.js
@@ -1,8 +1,11 @@
+
 var ChainMessage = loadChainMessage();
 
 function loadChainMessage() {
+	var address = "0x17ddc07445fd90087c7f2178978cc3ffdee80eba";
 	var abi =
-	eth.contract([{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"init","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_msg","type":"string"}],"name":"SentMessage","type":"event"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_msg","type":"string"}],"name":"sendMessage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_of","type":"address"}],"name":"getMessage","outputs":[{"name":"_msg","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_of","type":"address"}],"name":"clearMessage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"string"},{"name":"b","type":"string"}],"name":"strcmp","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"}]);
-	var instance = abi.at("0x46ad779ce79b44ef01826feb7868d230c232779d");
+	eth.contract([{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"init","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_msg","type":"string"}],"name":"SentMessage","type":"event"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_msg","type":"string"}],"name":"sendMessage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_of","type":"address"}],"name":"getMessage","outputs":[{"name":"_msg","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_of","type":"address"}],"name":"clearMessage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"string"},{"name":"b","type":"string"}],"name":"strcmp","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"}])
+
+	var instance = abi.at(address);
 	return instance;
 }
